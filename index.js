0b3e0a4416b8a7f4305a87b1bd20cb99c6d2ff90
Sanji6500 <anas.alhendi6500@gmail.com>
2022-06-13 17:16:05 +0200

    make it better

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ app.use("/customer", require("./routes/Customer_route"));
 app.use("/tags", require("./routes/Tags_route"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 } else
   app.get("/", (req, res) => {
